bf3a70abd419359ce35f8ecba914281667580261
Isaias Ulevich <101473921+Isaiasulevich@users.noreply.github.com>
2022-10-25 20:04:02 -0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PortfolioLogo from '../public/assets/img/portfolio-logo.svg'
+
+const Header = () => {
+  return (
+    <div className='py-4 bg-black'>
+
+        <img src={PortfolioLogo}/> 
+
+
+        
+    </div>
+  )
+}
+
+export default Header
