0174b535c2f52625336ac029045c7f29e218f38c
Isaias Ulevich <101473921+Isaiasulevich@users.noreply.github.com>
2022-10-25 21:29:20 -0300

    Add link to header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import portfolioLogo from '../public/assets/img/portfolio-logo.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className='py-4 px-8'>
+    <div className='py-4 px-24 flex justify-between items-center '>
 
         <Image 
          src={portfolioLogo}
          alt='The best logo I ever created'
-
          /> 
-
+        
+          <Link href={'/contact'} className='inline-flex text-white rounded-lg py-3 hover:text-indigo-500 active:text-white active:bg-'>
+            My Work
+          </Link>
     </div>
   )
 }
